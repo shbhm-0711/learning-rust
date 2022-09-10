8e9449321498ad00759f6fb92424b9dfd7dfa0ed
Shubham Singh <N1046810@my.ntu.ac.uk>
2022-09-10 18:51:41 +0530

    init commit

---
+++ 001_hello_world/hello_world.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("hello wormld hehe") // \!-shows its a micro
+}
+
+//----------------------------
+
+//******************** */
+// rustc ./hello_world.rs
+//******************** */
+// above line will generate binary(.exe) after compiling current file.
+// to compile, use rustc filename.rs
+
+//----------------------------
+// run bnary using:
+
+//******************** */
+// ./hello_world.exe
+//******************** */
