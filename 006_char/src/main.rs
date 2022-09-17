cc8c7bdd0a8c7f8a9e06ddd8e0a7933da43c99e6
Shubham Singh <N1046810@my.ntu.ac.uk>
2022-09-17 21:27:08 +0530

    characters

---
+++ 006_char/src/main.rs
@@ -0,0 +1,15 @@
+/*
+* unicode scalar value
+*
+
+*/
+
+fn main() {
+    let c = 'z';
+    let z = 'Z';
+    // let thak_gya_hu_bro_emoji = '😮‍💨';
+    let laugh = '😂';
+    println!("{}", c);
+    println!("{}", z);
+    println!("{}", laugh);
+}
